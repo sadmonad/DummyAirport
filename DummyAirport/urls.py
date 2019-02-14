4b0b0f39603a2ba2d590257d8de70d7678beb885
sadmonad <sadmonad@gmail.com>
2019-02-14 15:05:30 +0300

    Login

--- DummyAirport/urls.py
+++ DummyAirport/urls.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.contrib.auth.views import LoginView
+from django.urls import path, include
+
+import passengers.urls
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('', include(passengers.urls)),
 ]
